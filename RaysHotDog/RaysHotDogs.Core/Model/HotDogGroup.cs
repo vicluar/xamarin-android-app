b69e738668b181ac4340988aa638c61dc520320f
Vicluar Merchán <vicluar27@hotmail.com>
2018-08-28 23:49:36 +0200

    Adding basic elements for core library.

---
+++ RaysHotDog/RaysHotDogs.Core/Model/HotDogGroup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RaysHotDogs.Core.Model
+{
+    public class HotDogGroup
+    {
+        public int HotDogGroupId { get; set; }
+        public string Title { get; set; }
+        public string ImagePath { get; set; }
+        public List<HotDog> HotDogs { get; set; }
+    }
+}
---
+++ RaysHotDog/RaysHotDogs.Core/Service/HotDogDataService.cs
@@ -0,0 +1,38 @@
+﻿using RaysHotDogs.Core.Model;
+using RaysHotDogs.Core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RaysHotDogs.Core.Service
+{
+    public class HotDogDataService
+    {
+        private static HotDogRepository hotDogRepository = new HotDogRepository();
+
+        public List<HotDog> GetAllHotDogs()
+        {
+            return hotDogRepository.GetAllHotDogs();
+        }
+
+        public List<HotDogGroup> GetGroupedHotDogs()
+        {
+            return hotDogRepository.GetGroupedHotDogs();
+        }
+
+        public List<HotDog> GetHotDogsForGroup(int hotDogGroupId)
+        {
+            return hotDogRepository.GetHotDogsForGroup(hotDogGroupId);
+        }
+
+        public List<HotDog> GetFavoriteHotDogs()
+        {
+            return hotDogRepository.GetFavoriteHotDogs();
+        }
+
+        public HotDog GetHotDogById(int hotDogId)
+        {
+            return hotDogRepository.GetHotDogById(hotDogId);
+        }
+    }
+}
